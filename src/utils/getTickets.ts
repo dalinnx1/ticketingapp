fa558efb84a33025ca0c9568db8f288857f20262
dalin <dalinnx1@gmail.com>
2024-11-18 01:42:23 +0300

    init

---
+++ src/utils/getTickets.ts
@@ -0,0 +1,20 @@
+export async function getTicket(id?: string) {
+  try {
+    const res = await fetch(
+      `${process.env.PUBLIC_URL}/api/tickets/${id ? id : ""}`,
+      {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        cache: "no-store",
+      }
+    );
+    if (!res.ok) throw new Error("Failed to fetch topic");
+    const data = await res.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+    return { error: "Failed to fetch ticket data" };
+  }
+}
